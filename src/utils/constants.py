48f3401448b2d506780a4c49f61e49af29d8bf23
Felipe Fadul <felipe.fadul@hotmail.com>
2022-05-16 09:58:46 -0300

    fix: Change STOP_FRAME_QUANTITY to 1000
    It is used to stop the video at this frame when SILENT_MODE = True

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -2,7 +2,7 @@ OBJECT_ID_TO_DEBUG = 0
 KEYFRAME_INTERVAL = 4
 MINIMUM_CONFIDENCE = 0.45
 SLEEP_TIME_IN_SECONDS = 0.1
-STOP_FRAME_QUANTITY = 579
+STOP_FRAME_QUANTITY = 1000  # It is used to stop the video at this frame when SILENT_MODE = True
 VIDEO_ACCELERATION_SPEED = 50
 DEBUG_MODE = False  # If True, enable more drawings on the video to make debugging easier
 SILENT_MODE = False  # If True, run in the console, without graphical interface and stop when it reaches the frame defined by STOP_FRAME_QUANTITY
